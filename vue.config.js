a6397c0fda07e061133950227ac45f1c480ddaa5
Eleni <eleni_133@hotmail.com>
2024-09-29 15:40:47 +0100

    (chore): install dependencies

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,4 @@
-const path = require('path')
+const path = require("path");
 
 module.exports = {
   devServer: {
@@ -11,13 +11,21 @@ module.exports = {
       },
     },
   },
-  chainWebpack: config => {
-    config.plugin('copy').tap(args => {
+  chainWebpack: (config) => {
+    config.plugin("copy").tap((args) => {
       const UNESCAPED_GLOB_SYMBOLS_RE = /(\\?)([()*?[\]{|}]|^!|[!+@](?=\())/g;
-      const publicDir = path.resolve(process.VUE_CLI_SERVICE.context, 'public').replace(/\\/g, '/');
-      const escapePublicDir= publicDir.replace(UNESCAPED_GLOB_SYMBOLS_RE, '\\$2');
-      args[0].patterns[0].globOptions.ignore = args[0].patterns[0].globOptions.ignore.map(i => i.replace(publicDir, escapePublicDir));
+      const publicDir = path
+        .resolve(process.VUE_CLI_SERVICE.context, "public")
+        .replace(/\\/g, "/");
+      const escapePublicDir = publicDir.replace(
+        UNESCAPED_GLOB_SYMBOLS_RE,
+        "\\$2"
+      );
+      args[0].patterns[0].globOptions.ignore =
+        args[0].patterns[0].globOptions.ignore.map((i) =>
+          i.replace(publicDir, escapePublicDir)
+        );
       return args;
-  });
-  }
+    });
+  },
 };
