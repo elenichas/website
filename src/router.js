107f1438dda4c62f8eeff9aec5e4ef5b2fd0ced7
Eleni <eleni_133@hotmail.com>
2024-10-04 10:54:19 +0100

    (feat): move to vue3 and use vutify

--- src/router.js
+++ src/router.js
@@ -1,26 +1,26 @@
-import Vue from 'vue'
-import Router from 'vue-router'
-import Meta from 'vue-meta'
+import { createRouter, createWebHistory } from "vue-router";
+import Home from "./views/home.vue";
+import NotFound from "./views/not-found.vue";
+import "./style.css";
 
-import Home from './views/home'
-import NotFound from './views/not-found'
-import './style.css'
+// Define your routes
+const routes = [
+  {
+    name: "Home",
+    path: "/",
+    component: Home,
+  },
+  {
+    name: "404 - Not Found",
+    path: "/:pathMatch(.*)*", // This is the Vue 3 equivalent for catching all unmatched routes
+    component: NotFound,
+  },
+];
 
-Vue.use(Router)
-Vue.use(Meta)
-export default new Router({
-  mode: 'history',
-  routes: [
-    {
-      name: 'Home',
-      path: '/',
-      component: Home,
-    },
-    {
-      name: '404 - Not Found',
-      path: '**',
-      component: NotFound,
-      fallback: true,
-    },
-  ],
-})
+// Create the router instance
+const router = createRouter({
+  history: createWebHistory(), // Replaces `mode: 'history'` in Vue 3
+  routes,
+});
+
+export default router;
