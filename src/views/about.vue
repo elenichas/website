2e3859677e69307c58a66156b118321117f999cb
Eleni <eleni_133@hotmail.com>
2024-10-15 13:54:24 +0100

    (feat): establish main routes

---
+++ src/views/about.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="about">
+    <BackToHome />
+    <!-- Add this line to include the back button -->
+    <h1>About Us</h1>
+    <p>Find out more about our team and our mission.</p>
+  </div>
+</template>
+
+<script>
+import BackToHome from "../components/backToHome.vue";
+
+export default {
+  name: "About",
+  components: {
+    BackToHome, // Register the component
+  },
+};
+</script>
+
+<style scoped>
+.about {
+  text-align: center;
+  margin-top: 50px;
+}
+</style>
