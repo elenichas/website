502008500530843d3e9b6b3b975deeb649b6840f
Eleni <Eleni@MacBook-Air.local>
2026-06-01 15:57:10 +0100

    (fix): restore production view

--- vite.config.js
+++ vite.config.js
@@ -1,30 +1,9 @@
 import { fileURLToPath, URL } from "node:url";
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
-import vitePluginBundleObfuscator from "vite-plugin-bundle-obfuscator";
 
 export default defineConfig({
-  plugins: [
-    vue(),
-    vitePluginBundleObfuscator({
-      apply: "build",
-      autoExcludeNodeModules: true,
-      log: false,
-      options: {
-        compact: true,
-        simplify: true,
-        stringArray: true,
-        stringArrayShuffle: true,
-        stringArrayThreshold: 0.75,
-        identifierNamesGenerator: "hexadecimal",
-        renameGlobals: false,
-        controlFlowFlattening: false,
-        deadCodeInjection: false,
-        debugProtection: false,
-        selfDefending: false,
-      },
-    }),
-  ],
+  plugins: [vue()],
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
